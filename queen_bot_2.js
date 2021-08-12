55802326d025849cc75c2b4ac5a3021165cdb78b
King Stuugie <63889853+Stuugie33@users.noreply.github.com>
2021-08-12 08:49:10 -0500

    Add files via upload

---
+++ queen_bot_2.js
@@ -0,0 +1,106 @@
+const tmi = require('tmi.js');
+const options = {
+    options: {
+        debug:true,
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true,
+    },
+    identity: {
+         username: 'her_majesty_the_queen',
+         password: 'oauth:jk7hruwnxv2owg30wysfoxc6jefgcq',
+    },
+    channels: ['KingStuugie']
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+const Add = require("./Add")
+const Print = require("./Print")
+let addObject = new Add()
+let printObject = new Print()
+const channel_name = 'KingStuugie'
+var pollCounter = 0
+var dict = {}
+//pollData = global.Array = []
+var kingsNod = true
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
+
+var myPath = `C:/the_bot/Poll_Outputs/`
+var records
+var pollName = ''
+var startPoll = true
+/**
+ * The following chat event 
+ */
+client.on("chat", (channel, user, message, self) => {
+
+    var pollMessage = message.split(' ')
+    var userPollEntry = pollMessage[1]
+    var regMessage = new RegExp(`[0-9]`)
+    if (user.mod || user['usertype'] === 'mod' || user['display-name'] === channel_name || self) {
+        try {
+            if (pollMessage[0].toLowerCase() === '!poll' && regMessage.test(pollMessage[1]) === false){
+                if (pollMessage[1].toLowerCase() !== 'end' ){
+                    pollName = pollMessage[1]
+                    startPoll = true
+                    client.say(channel_name, `Rate ${pollName} from 1 to 10 and help King Stuugie Rate this game`)
+                    client.say(channel_name, `Use Command !Poll # to submit your vote.`)
+                }
+                else {
+                    startPoll = false
+                    pollCounter = 0
+                    var dt = Date.now()
+                    console.log(`Path and name: ${myPath}${pollName}`)
+                    const csvWriter = new createCsvWriter({
+                        path: `C:/the_bot/Poll_Outputs/${pollName}${dt}.csv`,
+                        header: [
+                            {id: 'user_ID', title: 'User ID'},
+                            {id: 'rating', title: 'Rating'}
+                        ]
+                    })
+
+                    for (var dict_key in dict){
+                        console.log (`dict key: ${dict_key}`)
+                        console.log(`Dict[dict_key]: ${dict[dict_key]}`)
+                        records = [
+                            {user_ID: dict_key, rating: dict[dict_key]}
+                        ]  
+                    }
+                    try {
+                        csvWriter.writeRecords(records)
+                        .then(() => {
+                            console.log(`...CSV write Done`)
+                        })
+                    }
+                    catch{
+                        console.log(`Error: ${TypeError}`)
+                    }
+                }
+            }
+        }
+        catch {
+            console.log(`Error: ${console.error()}`)
+        }
+    }
+    try {
+    if (startPoll === true && regMessage.test(pollMessage[1]) === true){
+        if (pollMessage[1] < 1 || pollMessage[1] > 10){
+            client.say(channel_name,`${user['display-name']}, wtf?  How is ${pollMessage[1]} a number from 1 to 10?`)
+        }
+        else{
+            console.log(`User Name: ${user['display-name']} and User ID: ${user['user-id']}`)
+            gatherPollData(user['user-id'], userPollEntry, pollName)
+        }
+    }}catch{
+        console.log(TypeError)
+    }
+})
+
+function gatherPollData(userID, PollEntry, pollNm){
+    dict[userID] = PollEntry
+    console.log(`gatherPollData: ${userID} & ${dict[userID]}`)
+}
+
